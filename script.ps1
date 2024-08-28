57bc630a081e23c2c97ad6cb41136ce47ffbe7a7
S'Bussiso Dube <80188685+Sbussiso@users.noreply.github.com>
2024-08-28 03:36:25 +0000

    first

---
+++ script.ps1
@@ -0,0 +1,5 @@
+# Create a .txt file and write the message
+"script executed on Windows" | Out-File -FilePath "script_output.txt"
+
+# Notify the user
+Write-Host "The script has executed and created 'script_output.txt'."
---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a .txt file and write the message
+echo "script executed on linux" > script_output.txt
+
+# Notify the user
+echo "The script has executed and created 'script_output.txt'."
